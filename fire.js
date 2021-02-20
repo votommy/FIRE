ed4dc54b910afed2352329e591ee86f8758f9343
Tommy Vo <votommy@outlook.com>
2021-02-20 14:12:04 -0600

    Added inflation rate

--- fire.js
+++ fire.js
@@ -1,38 +1,50 @@
-//Automatically update copyright year
-let year = new Date().getFullYear();
-document.getElementById("copyrightDate").innerHTML = "&copy;" + year + " Developed by Tommy Vo | All rights reserved";
-
-let retirementMarker = 1;
 function calculate() {
     const getSpending = document.getElementById("spending");
     const getInvestment = document.getElementById("investment");
     const getAnnualContribution = document.getElementById("annualContribution");
     const getReturnRate = document.getElementById("returnRate");
-    const retirementGoal = (getSpending.value / (getReturnRate.value / 100)) * 12;
+    const getInflationRate = document.getElementById("inflationRate");
     const getAge = document.getElementById("age");
     
     let spending = getSpending.value;
     if (spending.length == 0) {
         spending = 0.0;
     }
+    
     let investment = getInvestment.value;
     if (investment.length == 0) {
         investment = 0.0;
     }
+    
     let annualContribution = getAnnualContribution.value;
     if (annualContribution.length == 0) {
         annualContribution = 0.0;
     }
-    const returnRate = (getReturnRate.value / 100) + 1;
+    
+    let inflationRate = getInflationRate.value;
+    if (inflationRate.length == 0) {
+        inflationRate = 2.52;
+    }
+    inflationRate /= 100;
+    
+    let returnRate = getReturnRate.value;
+    if (returnRate.length == 0) {
+        returnRate = 0.0;
+    }
+    returnRate /= 100;
+    returnRate = (returnRate - inflationRate) + 1;
+    
     let age = getAge.value;
     if (age.length == 0) {
         age = 0;
     }
     
     let year = new Date().getFullYear();
+    const currency = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
     let count = 0;
     let xtraDigit = "0";
-    retirementMarker = 2;
+    const retirementGoal = (spending / (returnRate - 1)) * 12;
+    let retirementMarker = 2;
     let retireYet = 0;
     let endAge = 65;
     let previousInvestment = 0;
@@ -53,7 +65,7 @@ function calculate() {
                 endAge = age + 10;
             }
         }
-        let labelCol = "<tr><td>" + xtraDigit.concat(count) + "</td><td>Age " + age + "</td><td>" + year + "</td><td>" + new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(investment) + "</td><td>" + new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(gains) + "</td></tr>"
+        let labelCol = "<tr><td>" + xtraDigit.concat(count) + "</td><td>Age " + age + "</td><td>" + year + "</td><td>" + currency.format(investment) + "</td><td>" + currency.format(gains) + "</td></tr>"
         text += labelCol;                    
         investment *= returnRate;
         investment = parseFloat(investment) + parseFloat(annualContribution);
@@ -64,52 +76,51 @@ function calculate() {
     }
     text += "</table>";
     outputTable.innerHTML = text;
-    highlightRetirement();
+    
+    if (investment >= retirementGoal) {
+        document.querySelector("tr:nth-child(" + retirementMarker + ")").style.color = "var(--tommagenta)";
+        document.querySelector("tr:nth-child(" + retirementMarker + ")").style.fontWeight = "bold";
+    }
 }
-//Highlight the retirement row
-$(document).ready(function() { //jQuery
-    $(function() { 
-        function jsFunc(){ 
-            $("tr:nth-child(" + retirementMarker + ")").css("color", "var(--tommagenta)");
-            $("tr:nth-child(" + retirementMarker + ")").css("font-weight", "bold");
-        }  
-        highlightRetirement = jsFunc;
-    })
-}); //End jQuery
 
-const input1 = document.getElementById("spending");
-const input2 = document.getElementById("investment");
-const input3 = document.getElementById("annualContribution");
-const input4 = document.getElementById("returnRate");
-const input5 = document.getElementById("age");
 //Enter key activates Calculate button
-input1.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("button").click();
-  }
+document.getElementById("spending").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("button").click();
+    }
 });
-input2.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("button").click();
-  }
+document.getElementById("investment").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("button").click();
+    }
 });
-input3.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("button").click();
-  }
+document.getElementById("annualContribution").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("button").click();
+    }
 });
-input4.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("button").click();
-  }
+document.getElementById("returnRate").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("button").click();
+    }
 });
-input5.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("button").click();
-  }
+document.getElementById("inflationRate").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("button").click();
+    }
 });
+document.getElementById("age").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("button").click();
+    }
+});
+
+//Automatically update copyright year
+const copyrightYear = new Date().getFullYear();
+document.getElementById("copyrightDate").innerHTML = "&copy;" + copyrightYear + " Developed by Tommy Vo | All rights reserved";
