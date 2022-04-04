79c93d07f9822c135be370bd08e5d3bf5d43c173
Tommy Vo <votommy@outlook.com>
2022-04-04 02:46:57 -0500

    Added compatibility with edge cases

--- fire.js
+++ fire.js
@@ -51,6 +51,8 @@ function calculate() {
     let endAge = 65;
     let previousInvestment = 0;
     let gains = 0;
+    let failInvestment = 0;
+    let failYear = 0;
     
     let retirementAge = 0;
     let retirementYear = 0;
@@ -62,6 +64,10 @@ function calculate() {
         previousInvestment = investment;
         if (count > 9) {
             xtraDigit = "";
+        }
+        if (age == 65) { //For when financial independence isn't achieved
+            failInvestment = investment;
+            failYear = year;
         }
         if (investment >= retirementGoal && retireYet == 0) {
             retirementMarker = count + 2; // +2 to compensate for header row and the fact that index starts with 1
@@ -84,13 +90,16 @@ function calculate() {
     text += "</table><hr><span style='font-weight: bold;'>Disclaimer: </span><span style='font-style: italic;'>This program is only for theoretical application only, and should not be used for financial advice; consult a professional for real retirement advice & guidance.</span>";
     outputTable.innerHTML = text;
     
-    if (investment >= retirementGoal) {
+    if (retireYet == 1) {
+        document.querySelector("#outputParagraph").innerHTML = "<hr>Given the information you provided, you should be able to retire in <span style='font-weight: bold;'>" + retirementYear + "</span> at the age of <span style='font-weight: bold;'>" + retirementAge + "</span>.<br><br>Here\'s how it works:<br>During that year, your investments can generate enough money per year for you to live off of without having to work, theoretically.<br>Your retirement goal is to have at least <span style='font-weight: bold;'>" + currency.format(retirementGoal) + "</span> invested with a consistent return rate of <span style='font-weight: bold;'>" + retirementReturnRate + "%</span> (before inflation).<hr>";
+        
         document.querySelector("tr:nth-child(" + retirementMarker + ")").style.color = "#808000";
         document.querySelector("tr:nth-child(" + retirementMarker + ")").style.backgroundColor = "#FFFF99";
         document.querySelector("tr:nth-child(" + retirementMarker + ")").style.fontWeight = "bold";
     }
-    
-    document.querySelector("#outputParagraph").innerHTML = "<hr>Given the information you provided, you should be able to retire in <span style='font-weight: bold;'>" + retirementYear + "</span> at the age of <span style='font-weight: bold;'>" + retirementAge + "</span>.<br><br>Here\'s how it works:<br>During that year, your investments can generate enough money per year for you to live off of without having to work, theoretically.<br>Your retirement goal is to have at least <span style='font-weight: bold;'>" + currency.format(retirementGoal) + "</span> invested with a consistent return rate of <span style='font-weight: bold;'>" + retirementReturnRate + "%</span> (before inflation).<hr>";
+    else {
+        document.querySelector("#outputParagraph").innerHTML = "<hr>Given the information you provided, you cannot achieve financial independence before the age of 65.<br><br>In order to achieve financial independence, you must have at least <span style='font-weight: bold;'>" + currency.format(retirementGoal) + "</span> invested with a consistent return rate of <span style='font-weight: bold;'>" + retirementReturnRate + "%</span> (before inflation).<br>In " + failYear + ", when you are 65, you would have invested <span style='font-weight: bold;'>" + currency.format(failInvestment) + "</span>, which is less than what you'd need.<br><br>There's a combination of three things you can do to reach financial independence sooner:<br>1. Lower your monthly spending<br>2. Increase your annual contribution<br>3. Choose investments with a higher return rate<hr>";
+    }
 }
 
 //Enter key activates Calculate button
